a964cc75d08c180139bc2afc2ef6f8b227bc587f
Aruna Maurya <arunamaurya1994@gmail.com>
2023-03-20 20:43:28 +0530

    Checking commit

---
+++ src/Generic/IAutoConstant.java
@@ -0,0 +1,10 @@
+package Generic;
+
+public interface IAutoConstant {
+
+	String chromeKey="webdriver.chrome.driver" ;
+	String chromePath="C:\\Softwares\\chromedriver.exe" ;
+	String xlPath="./Data/Input.xlsx";
+	String config_path ="C:\\selProject1\\Property\\config.properties";
+	String LoginScenario="LOGIN";
+}
